a535f9e9b964083324af6331ad35f875e8fa0465
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-01-18 06:22:54 +0000

    updated

---
+++ example/websites.js
@@ -0,0 +1,6 @@
+module.exports = [
+  {
+    url: 'https://www.google.com', // URL of service we'll be pining
+    timeout: 200 // threshold in milliseconds above which is considered degraded performance
+  }
+]
