bf245ba597f8ccdfcc8250820e25da3f708c78eb
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2018-05-23 08:21:53 +0000

    updated

--- src/Uptime.js
+++ src/Uptime.js
@@ -57,44 +57,45 @@ class Uptime extends EventEmitter{
     }
     
     monitor(websites){
-        websites.forEach(service => {
-          this.serviceStatus[service.url] = {
-            status: 'OPERATIONAL', // initialize all services as operational when we start
-            responseTimes: [], // array containing the responses times for last 3 pings
-            timeout: service.timeout // load up the timout from the config
-          }
-        
-          setInterval(() => {
-            this.pingService(service.url, (serviceResponse) => {
-              if (serviceResponse === 'OUTAGE' && this.serviceStatus[service.url].status !== 'OUTAGE') {
-                // only update and post to Slack on state change
-                this.serviceStatus[service.url].status = 'OUTAGE'
-                this.postToSlack(service.url)
-              } else {
-                let responseTimes = this.serviceStatus[service.url].responseTimes
-                responseTimes.push(serviceResponse)
-        
-                // check degraded performance if we have 3 responses so we can average them
-                if (responseTimes.length > 3) {
-                  // remove the oldest response time (beginning of array)
-                  responseTimes.shift()
-        
-                  // compute average of last 3 response times
-                  let avgResTime = responseTimes.reduce((a, b) => a + b, 0) / responseTimes.length
-                  let currService = this.serviceStatus[service.url]
-        
-                  if (avgResTime > currService.timeout && currService.status !== 'DEGRADED') {
-                    currService.status = 'DEGRADED'
-                    this.postToSlack(service.url)
-                  } else if (avgResTime < currService.timeout && currService.status !== 'OPERATIONAL') {
-                    currService.status = 'OPERATIONAL'
-                    this.postToSlack(service.url)
-                  }
+      websites.forEach(service => {
+        this.serviceStatus[service.url] = {
+          status: 'OPERATIONAL', // initialize all services as operational when we start
+          responseTimes: [], // array containing the responses times for last 3 pings
+          timeout: service.timeout // load up the timout from the config
+        }
+      
+        setInterval(() => {
+          this.pingService(service.url, (serviceResponse) => {
+            if (serviceResponse === 'OUTAGE' && this.serviceStatus[service.url].status !== 'OUTAGE') {
+              // only update and post to Slack on state change
+              this.serviceStatus[service.url].status = 'OUTAGE'
+              this.postToSlack(service.url)
+            } else {
+              let responseTimes = this.serviceStatus[service.url].responseTimes
+              responseTimes.push(serviceResponse)
+      
+              // check degraded performance if we have 3 responses so we can average them
+              if (responseTimes.length > 3) {
+                // remove the oldest response time (beginning of array)
+                responseTimes.shift()
+      
+                // compute average of last 3 response times
+                let avgResTime = responseTimes.reduce((a, b) => a + b, 0) / responseTimes.length
+                let currService = this.serviceStatus[service.url]
+      
+                if (avgResTime > currService.timeout && currService.status !== 'DEGRADED') {
+                  currService.status = 'DEGRADED'
+                  this.postToSlack(service.url)
+                } else if (avgResTime < currService.timeout && currService.status !== 'OPERATIONAL') {
+                  currService.status = 'OPERATIONAL'
+                  this.postToSlack(service.url)
                 }
               }
-            })
-          }, this.pingInterval)
-        })
+      
+            }
+          })
+        }, this.pingInterval)
+      })
     }
 }
 
